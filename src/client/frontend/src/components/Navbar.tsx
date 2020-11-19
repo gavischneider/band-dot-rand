a53bb29c428f8ab36784053bf60d756ba46c781a
Gavi Schneider <gavischneider@gmail.com>
2020-11-20 01:26:41 +0200

    Updated twitter url

--- src/client/frontend/src/components/Navbar.tsx
+++ src/client/frontend/src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
       "data-text",
       "I'm discovering new artists on band.rand()!"
     );
-    anchor.setAttribute("data-url", "https://www.banddotrand.com");
+    anchor.setAttribute("data-url", "https://www.banddotrand.software");
     anchor.setAttribute("data-via", "gschnei");
     anchor.setAttribute("data-hashtags", "bandDotRand");
     anchor.setAttribute("data-lang", "en");
