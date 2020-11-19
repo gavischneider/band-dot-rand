d7ceff8aaec7631853c5c0720790a59f4ca94cc6
Gavi Schneider <gavischneider@gmail.com>
2020-11-19 23:13:29 +0200

    Added social icons

--- src/client/frontend/src/components/Footer.tsx
+++ src/client/frontend/src/components/Footer.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => {
   const year: Number = new Date().getFullYear();
@@ -8,12 +14,35 @@ export const Footer = () => {
       <div className="flex items-center justify-center flex-wrap bg-purple-500 p-6 shadow-lg inset-x-0 top-0 object-top sticky z-10">
         {`© ${year}`}
         <a
-          href="https://github.com/gavischneider"
+          href="https://gavischneider.github.io/Personal-Site/"
           target="_blank"
-          className="pl-2"
+          className="pl-1"
         >
           Gavi Schneider
         </a>
+        <div className="social-icons">
+          <a
+            href="https://www.linkedin.com/in/gavi-schneider-27a87837/"
+            target="_blank"
+            className="linkedin social pl-1"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+          <a
+            href="https://twitter.com/gschnei"
+            target="_blank"
+            className="twitter social pl-1"
+          >
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+          <a
+            href="https://github.com/gavischneider"
+            target="_blank"
+            className="github social pl-1"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+        </div>
       </div>
     </div>
   );
