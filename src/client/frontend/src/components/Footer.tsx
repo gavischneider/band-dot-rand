caf629b6bdab46cb6fe2a10e921503eb272a3bbc
Gavi Schneider <gavischneider@gmail.com>
2020-11-20 00:26:28 +0200

    Added tweet button

--- src/client/frontend/src/components/Footer.tsx
+++ src/client/frontend/src/components/Footer.tsx
@@ -4,6 +4,7 @@ import {
   faTwitter,
   faLinkedin,
   faGithub,
+  faDev,
 } from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => {
@@ -11,7 +12,7 @@ export const Footer = () => {
 
   return (
     <div className="pt-20">
-      <div className="flex items-center justify-center flex-wrap bg-purple-500 p-6 shadow-lg inset-x-0 top-0 object-top sticky z-10">
+      <div className="flex items-center justify-center flex-wrap bg-purple-500 p-3 shadow-lg inset-x-0 top-0 object-top sticky z-10">
         {`© ${year}`}
         <a
           href="https://gavischneider.github.io/Personal-Site/"
@@ -24,23 +25,30 @@ export const Footer = () => {
           <a
             href="https://www.linkedin.com/in/gavi-schneider-27a87837/"
             target="_blank"
-            className="linkedin social pl-1"
+            className="linkedin social pl-2"
           >
             <FontAwesomeIcon icon={faLinkedin} size="2x" />
           </a>
           <a
             href="https://twitter.com/gschnei"
             target="_blank"
-            className="twitter social pl-1"
+            className="twitter social pl-2"
           >
             <FontAwesomeIcon icon={faTwitter} size="2x" />
           </a>
           <a
             href="https://github.com/gavischneider"
             target="_blank"
-            className="github social pl-1"
+            className="github social pl-2"
           >
             <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          <a
+            href="https://dev.to/gschnei"
+            target="_blank"
+            className="dev social pl-2"
+          >
+            <FontAwesomeIcon icon={faDev} size="2x" />
           </a>
         </div>
       </div>
--- src/client/frontend/src/components/Navbar.tsx
+++ src/client/frontend/src/components/Navbar.tsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import imagePath from "../assets/logo2.png";
 
 export const Navbar = () => {
+  useEffect(() => {
+    const anchor = document.createElement("a");
+    anchor.setAttribute(
+      "href",
+      "https://twitter.com/share?ref_src=twsrc%5Etfw"
+    );
+    anchor.setAttribute("class", "twitter-share-button");
+    anchor.setAttribute("data-size", "large");
+    anchor.setAttribute(
+      "data-text",
+      "I'm discovering new artists on band.rand()!"
+    );
+    anchor.setAttribute("data-url", "https://www.banddotrand.com");
+    anchor.setAttribute("data-via", "gschnei");
+    anchor.setAttribute("data-hashtags", "bandDotRand");
+    anchor.setAttribute("data-lang", "en");
+    anchor.setAttribute("data-show-count", "false");
+    document.getElementsByClassName("twitter-embed")[0].appendChild(anchor);
+
+    const script = document.createElement("script");
+    script.setAttribute("src", "https://platform.twitter.com/widgets.js");
+    document.getElementsByClassName("twitter-embed")[0].appendChild(script);
+  }, []);
+
   return (
     <nav className="flex items-center justify-between flex-wrap bg-purple-500 pl-6 pr-6 pt-3 pb-3 shadow-lg inset-x-0 top-0 object-top sticky z-10">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <img src={imagePath} alt="logo" className="w-24 h-18" />
       </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
+      <div className="twitterContainer shadow-lg">
+        <div className="twitter-embed"></div>
       </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto"></div>
     </nav>
   );
 };
