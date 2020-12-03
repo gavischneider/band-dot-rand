5ebec34358dfb69b6e0b93648ad51ab8a874843b
Gavi Schneider <gavischneider@gmail.com>
2020-12-03 18:54:07 +0200

    Switched spotify button

--- src/client/frontend/src/components/ArtistsRelatedArtists.tsx
+++ src/client/frontend/src/components/ArtistsRelatedArtists.tsx
@@ -17,20 +17,19 @@ export const ArtistsRelatedArtists = (props: any) => {
   return (
     <div className="container mx-auto pt-10">
       <h1 className="mb-10 mt-8 text-2xl text-white">Related Artists</h1>
-      <div className="{width: 1, itemsToShow: 1},">
-        <Carousel breakPoints={breakpoints} className="p-5">
-          {relatedArtists.map((artist: any) => {
-            return (
-              <RelatedArtist
-                key={artist.id}
-                name={artist.name}
-                images={artist.images}
-                url={artist.external_urls.spotify}
-              />
-            );
-          })}
-        </Carousel>
-      </div>
+
+      <Carousel breakPoints={breakpoints} className="p-5">
+        {relatedArtists.map((artist: any) => {
+          return (
+            <RelatedArtist
+              key={artist.id}
+              name={artist.name}
+              images={artist.images}
+              url={artist.external_urls.spotify}
+            />
+          );
+        })}
+      </Carousel>
     </div>
   );
 };
