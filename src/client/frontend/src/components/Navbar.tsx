5208b6b923671d97bd8d9ad6bdaf78c1d374a72a
Gavi Schneider <gavischneider@gmail.com>
2020-12-03 20:49:27 +0200

    Added next button

--- src/client/frontend/src/components/Navbar.tsx
+++ src/client/frontend/src/components/Navbar.tsx
@@ -26,13 +26,27 @@ export const Navbar = () => {
     document.getElementsByClassName("twitter-embed")[0].appendChild(script);
   }, []);
 
+  function handleClick() {
+    window.location.reload();
+  }
+
   return (
     <nav className="flex items-center justify-between flex-wrap bg-purple-600 pl-6 pr-6 pt-3 pb-3 shadow-lg inset-x-0 top-0 object-top sticky z-10">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <img src={imagePath} alt="logo" className="w-24 h-18" />
       </div>
-      <div className="twitterContainer shadow-lg">
-        <div className="twitter-embed"></div>
+      <div className="flex">
+        <div className="my-auto pr-1">
+          <button
+            onClick={handleClick}
+            className="h-10 px-5 m-2 text-purple-100 transition-colors duration-150 bg-purple-800 rounded-lg focus:shadow-outline hover:bg-purple-900 shadow-lg"
+          >
+            Next!
+          </button>
+        </div>
+        <div className="twitterContainer shadow-lg">
+          <div className="twitter-embed"></div>
+        </div>
       </div>
     </nav>
   );
