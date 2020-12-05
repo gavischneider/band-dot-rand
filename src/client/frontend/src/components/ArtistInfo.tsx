7468ef7b3b10af206bee3d7ca8b33c11ab1899aa
Gavi Schneider <gavischneider@gmail.com>
2020-12-05 13:05:30 +0200

    Moved tweet button off of navbar

--- src/client/frontend/src/components/ArtistInfo.tsx
+++ src/client/frontend/src/components/ArtistInfo.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ArtistPhoto } from "./ArtistPhoto";
 import imagePath from "../assets/spotifybutton.png";
 
 export const ArtistInfo = (props: any) => {
-  console.log("PROPS");
-  console.log(props);
+  //console.log("PROPS");
+  //console.log(props);
+
+  useEffect(() => {
+    const anchor = document.createElement("a");
+    anchor.setAttribute(
+      "href",
+      "https://twitter.com/share?ref_src=twsrc%5Etfw"
+    );
+    anchor.setAttribute("class", "twitter-share-button");
+    anchor.setAttribute("data-size", "large");
+    anchor.setAttribute(
+      "data-text",
+      "I'm discovering new artists on band.rand()!"
+    );
+    anchor.setAttribute("data-url", "https://www.banddotrand.software");
+    anchor.setAttribute("data-via", "gschnei");
+    anchor.setAttribute("data-hashtags", "bandDotRand");
+    anchor.setAttribute("data-lang", "en");
+    anchor.setAttribute("data-show-count", "false");
+    document.getElementsByClassName("twitter-embed")[0].appendChild(anchor);
+
+    const script = document.createElement("script");
+    script.setAttribute("src", "https://platform.twitter.com/widgets.js");
+    document.getElementsByClassName("twitter-embed")[0].appendChild(script);
+  }, []);
 
   return (
     <div className="lg:flex justify-center py-12 px-12 container mx-auto rounded mt-5 mb-8 shadow-lg bg-gray-800">
@@ -21,7 +45,7 @@ export const ArtistInfo = (props: any) => {
               rel="noreferrer"
             >
               <img
-                className="w-40 shadow-lg transform transition duration-300 hover:scale-110"
+                className="w-40 shadow-lg transform transition duration-300 hover:scale-110 mb-6"
                 alt="spotify button"
                 src={imagePath}
               />
@@ -29,6 +53,9 @@ export const ArtistInfo = (props: any) => {
           ) : (
             <h3>""</h3>
           )}
+          <div className="twitterContainer">
+            <div className="twitter-embed"></div>
+          </div>
         </div>
       </div>
     </div>
--- src/client/frontend/src/components/Navbar.tsx
+++ src/client/frontend/src/components/Navbar.tsx
@@ -2,30 +2,6 @@ import React, { useEffect } from "react";
 import imagePath from "../assets/logo2.png";
 
 export const Navbar = () => {
-  useEffect(() => {
-    const anchor = document.createElement("a");
-    anchor.setAttribute(
-      "href",
-      "https://twitter.com/share?ref_src=twsrc%5Etfw"
-    );
-    anchor.setAttribute("class", "twitter-share-button");
-    anchor.setAttribute("data-size", "large");
-    anchor.setAttribute(
-      "data-text",
-      "I'm discovering new artists on band.rand()!"
-    );
-    anchor.setAttribute("data-url", "https://www.banddotrand.software");
-    anchor.setAttribute("data-via", "gschnei");
-    anchor.setAttribute("data-hashtags", "bandDotRand");
-    anchor.setAttribute("data-lang", "en");
-    anchor.setAttribute("data-show-count", "false");
-    document.getElementsByClassName("twitter-embed")[0].appendChild(anchor);
-
-    const script = document.createElement("script");
-    script.setAttribute("src", "https://platform.twitter.com/widgets.js");
-    document.getElementsByClassName("twitter-embed")[0].appendChild(script);
-  }, []);
-
   function handleClick() {
     window.location.reload();
   }
@@ -43,9 +19,6 @@ export const Navbar = () => {
           >
             Next!
           </button>
-        </div>
-        <div className="twitterContainer shadow-lg">
-          <div className="twitter-embed"></div>
         </div>
       </div>
     </nav>
