655b18e421d523f61da31e2e6a0b334b3de51fda
Gavi Schneider <gavischneider@gmail.com>
2020-12-05 16:45:52 +0200

    debug

--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 require("dotenv").config();
 const app = express_1.default();
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 const random = require("./routes/random");
 app.use("/random", random);
 // Allows frontend to call backend API
@@ -14,6 +15,15 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express_1.default.static("client/frontend/build"));
+    app.get("*", (req, res) => {
+        // Send request to the index page
+        res.sendFile(path.resolve(__dirname, "client", "frontend", "build", "index.html"));
+    });
+}
 // Home route
 app.get("/", (req, res) => {
     res.send("Welcome to bandDotRand! To get a random song, go to the '/random' route.");
--- dist/client/frontend/src/components/ArtistInfo.js
+++ dist/client/frontend/src/components/ArtistInfo.js
@@ -30,11 +30,12 @@ exports.ArtistInfo = (props) => {
     //console.log("PROPS");
     //console.log(props);
     react_1.useEffect(() => {
+        const name = props.info.name;
         const anchor = document.createElement("a");
         anchor.setAttribute("href", "https://twitter.com/share?ref_src=twsrc%5Etfw");
         anchor.setAttribute("class", "twitter-share-button");
         anchor.setAttribute("data-size", "large");
-        anchor.setAttribute("data-text", `I discovered ${props.info.name} on band.rand()!`);
+        anchor.setAttribute("data-text", `I discovered ${name} on band.rand()!`);
         anchor.setAttribute("data-url", "https://www.banddotrand.software");
         anchor.setAttribute("data-via", "gschnei");
         anchor.setAttribute("data-hashtags", "bandDotRand");
@@ -44,7 +45,7 @@ exports.ArtistInfo = (props) => {
         const script = document.createElement("script");
         script.setAttribute("src", "https://platform.twitter.com/widgets.js");
         document.getElementsByClassName("twitter-embed")[0].appendChild(script);
-    }, []);
+    }, [props.info.name]);
     return (react_1.default.createElement("div", { className: "lg:flex justify-center py-12 px-12 container mx-auto rounded mt-5 mb-8 shadow-lg bg-gray-800" },
         react_1.default.createElement("div", { className: "text-left lg:mt-4 lg:mt-0 lg:ml-10 flex content-center px-5" },
             react_1.default.createElement(ArtistPhoto_1.ArtistPhoto, { photo: props.info.images[1].url })),
